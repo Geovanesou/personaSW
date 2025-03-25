8d327f10c640345757dfbc4eb4439710b1aab727
GeovaneSou <geographix@gmail.com>
2025-03-25 19:22:32 -0300

    Aulas finais

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentPageUrl = 'https://swapi.dev/api/people/';
+let currentPageUrl = 'https://rickandmortyapi.com/api/character';
 
 window.onload = async () => {
   try {
@@ -9,9 +9,9 @@ window.onload = async () => {
   }
 
   const nextButton = document.getElementById('next-button')
-  const backButton = document.getElementById('back-button')
-
   nextButton.addEventListener('click', loadNextPage)
+  
+  const backButton = document.getElementById('back-button')
   backButton.addEventListener('click', loadPreviousPage)
 };
 
@@ -26,68 +26,67 @@ async function loadCharacters(url) {
 
     responseJson.results.forEach((character) => {
       const card = document.createElement("div");
-      card.style.backgroundImage = `url('https://web.archive.org/web/20230325201650if_/https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g, "")}.jpg')`;
+      card.style.backgroundImage = `url(${character.image})`;
       card.className = "cards";
-
       const characterNameBG = document.createElement("div");
       characterNameBG.className = "character-name-bg";
-
       const characterName = document.createElement("span");
       characterName.className = "character-name";
       characterName.innerText = `${character.name}`;
-
       characterNameBG.appendChild(characterName);
       card.appendChild(characterNameBG);
-
       card.onclick = () => {
         const modal = document.getElementById('modal');
         modal.style.visibility = 'visible';
-
         const modalContent = document.getElementById('modal-content');
         modalContent.innerHTML = '';
 
         const characterImage = document.createElement('div');
-        characterImage.style.backgroundImage = `url('https://web.archive.org/web/20230325201650if_/https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g, "")}.jpg')`;
+        characterImage.style.backgroundImage = `url(${character.image})`;
         characterImage.className = 'character-image';
 
         const name = document.createElement('span');
         name.className = 'character-details';
         name.innerText = `Nome: ${character.name}`;
 
-        const characterHeight = document.createElement('span');
-        characterHeight.className = 'character-details';
-        characterHeight.innerText = `Altura: ${convertHeight(character.height)}`;
+        const characterStatus = document.createElement('span');
+        characterStatus.className = 'character-details';
+        characterStatus.innerText = `Situação: ${convertStatus(
+          character.status
+        )}`;
 
-        const mass = document.createElement('span');
-        mass.className = 'character-details';
-        mass.innerText = `Peso: ${convertMass(character.mass)}`;
+        const species = document.createElement('span');
+        species.className = 'character-details';
+        species.innerText = `Espécie: ${convertSpecies(character.species)}`;
 
-        const eyeColor = document.createElement('span');
-        eyeColor.className = 'character-details';
-        eyeColor.innerText = `Cor dos olhos: ${convertEyeColor(character.eye_color)}`;
+        const gender = document.createElement('span');
+        gender.className = 'character-details';
+        gender.innerText = `Gênero: ${convertGender(
+          character.gender
+        )}`;
 
-        const birthYear = document.createElement("span")
-        birthYear.className = "character-details"
-        birthYear.innerText = `Nascimento: ${convertBirthYear(character.birth_year)}`
+        const origin = document.createElement("span")
+        origin.className = "character-details"
+        origin.innerText = `origem: ${character.origin.name}`;
 
         modalContent.appendChild(characterImage)
         modalContent.appendChild(name)
-        modalContent.appendChild(characterHeight)
-        modalContent.appendChild(mass)
-        modalContent.appendChild(eyeColor)
-        modalContent.appendChild(birthYear)
+        modalContent.appendChild(characterStatus)
+        modalContent.appendChild(species)
+        modalContent.appendChild(gender)
+        modalContent.appendChild(origin)
         };
 
+      const mainContent = document.getElementById('main-content');
       mainContent.appendChild(card);
     });
 
     const nextButton = document.getElementById('next-button');
     const backButton = document.getElementById('back-button');
+    nextButton.disabled = !responseJson.info.next;
+    backButton.disabled = !responseJson.info.prev;
 
-    nextButton.disabled = !responseJson.next
-    backButton.disabled = !responseJson.previous
-
-    backButton.style.visibility = responseJson.previous? "visible" : "hidden"
+    backButton.style.visibility = responseJson.info.prev? "visible" : "hidden"
 
     currentPageUrl = url
 
@@ -104,7 +103,7 @@ async function loadNextPage() {
     const response = await fetch(currentPageUrl);
     const responseJson = await response.json();
 
-    await loadCharacters(responseJson.next);
+    await loadCharacters(responseJson.info.next);
 
   } catch (error) {
     console.log(error);
@@ -119,7 +118,7 @@ async function loadPreviousPage() {
     const response = await fetch(currentPageUrl)
     const responseJson = await response.json()
 
-    await loadCharacters(responseJson.previous)
+    await loadCharacters(responseJson.info.prev)
 
   } catch (error) {
     console.log(error)
@@ -132,43 +131,45 @@ function hideModal() {
   modal.style.display = 'hidden';
 }
 
-function convertEyeColor(eyeColor) {
-  const cores = {
-    blue: "azul",
-    brown: "castanho",
-    green: "verde",
-    yellow: "amarelo",
-    black: "preto",
-    pink: "rosa",
-    red: "vermelho",
-    orange: "laranja",
-    hazel: "avelã",
-    unknown: "desconhecida"
+function convertStatus(status) {
+  const characterStatus = {
+    alive: "vivo",
+    dead: "morto",
+    unknown: "desconhecido"
   };
 
-  return cores[eyeColor.toLowerCase()] || eyeColor;
+  return characterStatus[status.toLowerCase()] || status;
 }
 
-function convertHeight(height) {
-  if (height === "unknown") {
-    return "desconhecida";
-  }
-  
-  return (height / 100).toFixed(2);
+function convertSpecies(specie) {
+  const characterSpecie = {
+    human: "humano",
+    alien: "alienígena",
+    humanoid: "humanóide",
+    animal: "animal",
+    "mythological creature": "criatura mitológica",
+    disease: "doença",
+    robot: "robô",
+    unknown: "desconhecido"
+  };
+
+  return characterSpecie[specie.toLowerCase()] || specie;
 }
 
-function convertMass(mass) {
-  if (mass === "unknown") {
+function convertGender(gender) {
+  const characterGender = {
+    male: "macho",
+    female: "fêmea",
+    unknown: "desconhecido"
+  };
+
+  return characterGender[gender.toLowerCase()] || gender;
+}
+
+function convertorigin(origin) {
+  if (origin === "unknown") {
     return "desconhecido";
   }
   
-  return `${mass} kg`;
-}
-
-function convertBirthYear(birthYear) {
-  if (birthYear === "unknown") {
-    return "desconhecido";
-  }
-  
-  return birthYear;
+  return origin;
 }
